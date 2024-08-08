09363cbb099d9c483ae4496fe70bf048b58e46f7
Sanchita0812 <sanchitakiran08@gmail.com>
2024-08-09 00:34:43 +0530

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -87,7 +87,7 @@ export default function Home() {
         open={open}
         onClose={handleClose}
         aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
+        aria-describedby="modal-modal-description" 
       >
         <Box sx={style}>
           <Typography id="modal-modal-title" variant="h6" component="h2">
